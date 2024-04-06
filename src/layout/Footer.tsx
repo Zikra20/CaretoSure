fa7768a7dd90905d629873541d011f0453a55a70
facuCast123 <facundo022020@gmail.com>
2024-04-06 19:19:25 -0300

    Added Navbar and Footer components.
    Created fully responsive Navbar and Footer components.

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,59 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>This is the footer.</div>;
+  return (
+    <div className="w-full px-4 py-8 flex flex-col justify-between gap-y-8 bg-indigo-950 lg:px-12 lg:py-16 lg:flex-row">
+      <div className="flex flex-col gap-y-4 lg:basis-1/4 lg:mr-auto">
+        <h1 className="text-2xl font-serif font-medium text-teal-500">
+          SrMedical
+        </h1>
+
+        <p className="text-lg text-purple-400">
+          Amet minim mollit non deserut adipisci iusto omnis cupiditate tempora,
+          ut itaque soluta vero sequi recusandae.
+        </p>
+
+        <div className="flex gap-x-4">
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            className="pl-4 text-purple-400 bg-transparent border border-purple-400 rounded-2xl focus:outline-0 placeholder:text-purple-400"
+          />
+          <button className="py-2 px-4 bg-teal-500 text-white rounded-xl md:hover:bg-teal-600 ease-in-out duration-200">
+            Subscribe
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-between gap-y-12 text-purple-400 lg:basis-2/4 lg:flex-nowrap lg:justify-between">
+        <div className="flex flex-col gap-y-6 [&>*]:w-fit">
+          <span className="text-2xl font-semibold">Menu</span>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/service">Service</Link>
+          <Link to="/doctors">Partner Team</Link>
+        </div>
+
+        <div className="flex flex-col gap-y-6 [&>*]:w-fit">
+          <span className="text-2xl font-semibold">Legal</span>
+          <a>Privacy Policy</a>
+          <a>Terms & Conditions</a>
+          <a>Service</a>
+          <a>Blog Post</a>
+        </div>
+
+        <div className="flex flex-col gap-y-6 font-medium [&>*]:w-fit">
+          <span className="text-2xl font-semibold">Contact</span>
+          <p>
+            2115 Thornridge Cir. Syracuse, <br />
+            Connecticut 36421
+          </p>
+          <p>legitdoctors@gmail.com</p>
+          <p>+1 387 894552</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import { IoMenu, IoClose } from "react-icons/io5";
+
 const Navbar = () => {
+  // Mobile menu display logic
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const handleMenu = () => {
+    setDisplayMenu((prev) => !prev);
+  };
+
   return (
-    <nav className="p-4">
-      <h1 className="text-xl font-serif font-medium text-teal-500">
+    <nav className="relative flex items-center p-4 md:justify-around">
+      <h1 className="mr-auto text-2xl font-serif font-medium text-teal-500 md:mr-0">
         SrMedical
       </h1>
+
+      <div className="hidden sm:flex gap-x-4 text-lg font-medium text-indigo-900">
+        <NavLink className="hover:text-indigo-700" to="/about">
+          About
+        </NavLink>
+        <NavLink className="hover:text-indigo-700" to="/services">
+          Services
+        </NavLink>
+        <NavLink className="hover:text-indigo-700" to="/doctors">
+          Doctors
+        </NavLink>
+        <NavLink className="hover:text-indigo-700" to="/blog">
+          Blog
+        </NavLink>
+        <NavLink className="hover:text-indigo-700" to="/contact">
+          Contact
+        </NavLink>
+      </div>
+
+      <button className="hidden md:block py-2 px-4 bg-teal-500 text-white rounded-2xl hover:bg-teal-600 ease-in-out duration-200">
+        Schedule Appointment
+      </button>
+
+      {/* Mobile menu */}
+      <div className="text-3xl sm:hidden" onClick={handleMenu}>
+        {displayMenu ? <IoClose /> : <IoMenu />}
+      </div>
+
+      {displayMenu && (
+        <div className="absolute top-full left-0 w-full flex flex-col gap-y-4 bg-white text-lg font-medium text-indigo-900 z-10 [&>*]:pt-4 [&>*]:pl-4 [&>*]:border-t md:hidden">
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/doctors">Doctors</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact" className="pb-4">
+            Contact
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
