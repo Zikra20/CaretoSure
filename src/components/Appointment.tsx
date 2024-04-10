1c6088779d32d136e3dbde3dad2c301aad44e90c
facuCast123 <facundo022020@gmail.com>
2024-04-10 10:56:24 -0300

    Fixed Appointments Component
    - Fixed a layout problem in Appointment.tsx

--- src/components/Appointment.tsx
+++ src/components/Appointment.tsx
@@ -2,21 +2,22 @@ import { IoArrowForward } from "react-icons/io5";
 
 const Appointment = () => {
   return (
-    <section className="py-8 px-4 flex flex-col items-center justify-center bg-purple-400 text-purple-900 lg:py-24 [&>*]:w-fit">
-      <h4 className="text-4xl font-semibold mr-auto md:basis-1/3">
+    <section className="py-8 px-4 flex flex-col items-center gap-y-4 bg-purple-400 text-purple-900 text-center lg:py-24 lg:gap-y-8 [&>*]:w-fit">
+      <h4 className="text-4xl font-semibold">
         Request your{" "}
         <span className="font-playfair-display text-white italic">
           appointment
         </span>
+        <br />
         today
       </h4>
 
-      <p>
+      <p className="max-w-[60%]">
         Ready to prioritize your health? Schedule an appointment with one of our
         experienced healthcare professionals today.
       </p>
 
-      <button className="w-fit py-4 px-8 self-start flex items-center gap-x-4 bg-teal-500 font-medium text-white rounded-2xl lg:hover:bg-teal-600 ease-in-out duration-200">
+      <button className="py-4 px-8 flex items-center gap-x-4 bg-teal-500 font-medium text-white rounded-2xl lg:hover:bg-teal-600 ease-in-out duration-200">
         Schedule an appointment <IoArrowForward className="text-2xl" />
       </button>
     </section>
