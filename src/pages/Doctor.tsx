afe7bc33a73756457c01addcab5a677195ec9708
facuCast123 <facundo022020@gmail.com>
2024-04-26 21:27:58 -0300

    Updated Doctor.tsx
    -Added fully responsive 'details' section.
    -Added react-icons imports used in the section.

--- src/pages/Doctor.tsx
+++ src/pages/Doctor.tsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 
+import { TbFirstAidKit } from "react-icons/tb";
+import { IoArrowForward } from "react-icons/io5";
+
 import doctorsData from "../data/doctorsData";
 
 const Doctor = () => {
@@ -7,6 +10,7 @@ const Doctor = () => {
 
   return (
     <>
+      {/* Header Section */}
       <section className="py-8 px-4 bg-indigo-950 text-purple-400 lg:px-16 lg:py-20">
         {doctorsData.map((doctor) => {
           if (doctor.id.toString() === params.doctorId)
@@ -46,6 +50,78 @@ const Doctor = () => {
               </div>
             );
         })}
+      </section>
+
+      {/* Details Section */}
+      <section className="py-8 px-4 text-purple-950 md:px-40 lg:py-20 lg:px-80">
+        <div className="mb-8 flex flex-col gap-y-4">
+          <p>
+            <span className="font-bold">Biography: </span>
+            Dr. Sudeshna Sinha is a seasoned rheumatologist with over 12 years
+            of dedicated experience in diagnosing and treating a wide range of
+            rheumatic diseases. With a passion for improving patients' quality
+            of life, Dr. Sinha has established himself as a leading expert in
+            the field. After completing him medical degree at OSU, Dr. Sinha
+            pursued specialized training in rheumatology, recognizing the
+            complexity and challenges of managing autoimmune and musculoskeletal
+            disorders. His commitment to excellence led him to undertake
+            rigorous residency and fellowship programs at renowned institutions,
+            where he honed his skills in the diagnosis and management of
+            conditions such as rheumatoid arthritis, lupus, osteoarthritis, and
+            gout, among others.
+          </p>
+          <p>
+            In addition to his clinical practice, Dr. Sinha is actively involved
+            in research and education, staying abreast of the latest
+            advancements in rheumatology to ensure his patients receive the most
+            cutting-edge care available. He has authored numerous publications
+            in peer-reviewed journals and frequently presents his research
+            findings at national and international conferences.
+          </p>
+          <p>
+            Dr. Sinha is board-certified in rheumatology and maintains
+            affiliations with several professional organizations, including the
+            American College of Rheumatology and the Rheumatology Research
+            Foundation. Patients seeking expert care and compassionate support
+            for rheumatic conditions can trust in Dr. Sudeshna Sinha's expertise
+            and dedication to improving their health and well-being.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-y-4">
+          <h2 className="text-2xl font-bold">Specialties</h2>
+
+          <div className="mb-8 flex flex-col gap-y-2 [&>span]:flex [&>span]:items-center [&>span]:gap-x-2 [&>span]:text-lg">
+            <span>
+              <TbFirstAidKit className="text-2xl" /> Dentistry
+            </span>
+
+            <span>
+              <TbFirstAidKit className="text-2xl" /> Cosmetic Dentistry
+            </span>
+
+            <span>
+              <TbFirstAidKit className="text-2xl" /> Emergency Dentistry
+            </span>
+
+            <span>
+              <TbFirstAidKit className="text-2xl" /> Laser Dentistry
+            </span>
+
+            <span>
+              <TbFirstAidKit className="text-2xl" /> Endodontics
+            </span>
+          </div>
+
+          <div className="flex flex-col gap-4 [&>*]:text-lg md:flex-row">
+            <button className="w-fit flex items-center gap-x-2 py-4 px-8 text-white font-semibold bg-teal-500 rounded-lg">
+              Make an Appointment <IoArrowForward className="text-xl" />
+            </button>
+            <div className="w-fit py-4 px-8 text-teal-500 font-semibold border-2 border-teal-500 rounded-lg">
+              +000 5687 4747
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
